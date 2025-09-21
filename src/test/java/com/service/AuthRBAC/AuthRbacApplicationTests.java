9ff69ecd7c35764796924fba985aed83e5b5bb85
ed <eduardodsxavier@gmail.com>
2025-09-21 18:11:16 -0300

    start spring-boot

---
+++ src/test/java/com/service/AuthRBAC/AuthRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.AuthRBAC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthRbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
